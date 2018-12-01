012be1d62cc49d45736069f2debc332049168855
Takaaki Hirayama <takaaki.hirayama@bravesoft.co.jp>
2018-12-01 17:55:50 +0900

    Unlink a configuration file for Redis Sentinel.

--- data/comp_conf.sh
+++ data/comp_conf.sh
@@ -1,2 +1,1 @@
-diff /vagrant_data/redis.conf /etc/redis.conf
 diff /vagrant_data/redis-sentinel.conf /etc/redis-sentinel.conf
--- data/sync_conf.sh
+++ data/sync_conf.sh
@@ -1,3 +1,2 @@
-cp -f /vagrant_data/redis.conf /etc/redis.conf
 cp -f /vagrant_data/redis-sentinel.conf /etc/redis-sentinel.conf
-chmod a+r /etc/redis.conf /etc/redis-sentinel.conf
+chmod a+rw /etc/redis.conf /etc/redis-sentinel.conf
